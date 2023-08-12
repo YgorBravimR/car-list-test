5ffec75c7c872c679a53dbaf68b36d66a67d87ea
Ygor Bravim INSIDE <ygor@insidethebox.com.br>
2023-08-12 14:17:58 -0300

    initial setup

---
+++ src/pages/index.tsx
@@ -0,0 +1,9 @@
+import styles from './home.module.scss'
+
+export default function Page() {
+  return (
+    <main className={styles.main}>
+      <h1 className={styles.title}>Hello world</h1>
+    </main>
+  )
+}
